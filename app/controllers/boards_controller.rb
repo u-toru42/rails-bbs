909e72db0d7c8460dfa859adb8934463970181de
u-toru42 <golt7040@gmail.com>
2019-07-20 09:42:31 +0900

    done

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,34 @@
 class BoardsController < ApplicationController
 	def index
+    @boards = Board.all
 	end
+
+  def new
+    @board = Board.new
+  end
+
+  def create
+    Board.create(board_params)
+  end
+
+  def show
+    @board = Board.find(params[:id])
+  end
+
+  def edit
+    @board = Board.find(params[:id])
+  end
+
+  def update
+    board = Board.find(params[:id])
+    board.update(board_params)
+
+    redirect_to board
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:name, :title, :body)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'boards#index'
+  resources :boards, only: [:index, :new, :create, :show, :edit, :update]
 end
